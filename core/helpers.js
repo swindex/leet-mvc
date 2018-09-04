211de66f41ebd749aa542d97b73664370a07a436
Eldar Gerfanov <zerodivide@mail.ru>
2018-09-04 08:53:26 -0400

    Added

--- core/helpers.js
+++ core/helpers.js
@@ -1,5 +1,4 @@
 import { isObject, isArray } from "util";
-
 /**
  * Check if object is empty
  * @param {*} value 
@@ -61,63 +60,7 @@ export function Override(context, method, callback){
 		return func.apply(this,arguments);
 	};
 };*/
-/**
- * This function creates proxy from any object and watches its changes. Triggers only when own properties change or properties of its simple properties
- * @param {*} object 
- * @param {function} onChangeCallback 
- */
-export function  onChange(object, onChangeCallback){
-	//var builtin = ['Object','Array','Date'];
-	const handler = {
-		get(target, property, receiver) {
-			const desc = Object.getOwnPropertyDescriptor(target, property)
-			const value = Reflect.get(target, property, receiver)
-			
-			//return non-modifiable objects as-is
-			if (desc && !desc.writable && !desc.configurable) return value
 
-			//return objects, instantiated with `new` as-is
-			if (! isArray(value) && isObject(value) && !isObjLiteral(value))
-				return value;
-			
-			//return as-is if its a primitive	
-			if (! isObject(value))
-				return value;
-
-			try {
-				return new Proxy(target[property], handler);
-			} catch (error) {
-				return value
-			}
-		},
-		defineProperty(target, property, descriptor) {
-			onChangeCallback(target,property);
-			return Reflect.defineProperty(target, property, descriptor);
-		},
-		deleteProperty(target, property) {
-			onChangeCallback(target,property);
-			return Reflect.deleteProperty(target, property);
-		}
-	};
-	return new Proxy(object, handler);
-};
-
-export function isObjLiteral(_obj) {
-	var _test  = _obj;
-	return (  typeof _obj !== 'object' || _obj === null ?
-				false :  
-				(
-				  (function () {
-					while (!false) {
-					  if (  Object.getPrototypeOf( _test = Object.getPrototypeOf(_test)  ) === null) {
-						break;
-					  }      
-					}
-					return Object.getPrototypeOf(_obj) === _test;
-				  })()
-				)
-			);
-  }
 
 export function GUID() {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
