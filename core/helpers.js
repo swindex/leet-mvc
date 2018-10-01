47a4214559c27d3c5117b31ab89c458243134ca6
Eldar Gerfanov <zero.divide.net@hotmail.com>
2018-09-30 20:47:33 -0400

    Added

--- core/helpers.js
+++ core/helpers.js
@@ -68,3 +68,20 @@ export function GUID() {
 		return v.toString(16);
 	});
 }
+
+export function isObjLiteral(_obj) {
+	var _test  = _obj;
+	return (  typeof _obj !== 'object' || _obj === null ?
+				false :  
+				(
+				  (function () {
+					while (!false) {
+					  if (  Object.getPrototypeOf( _test = Object.getPrototypeOf(_test)  ) === null) {
+						break;
+					  }      
+					}
+					return Object.getPrototypeOf(_obj) === _test;
+				  })()
+				)
+			);
+  }
