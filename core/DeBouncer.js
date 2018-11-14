9ff1b10c8eb16a51348f7174291d3b3f5183a944
Eldar Gerfanov <zerodivide@mail.ru>
2018-11-14 14:50:27 -0500

    Added

---
+++ core/DeBouncer.js
@@ -0,0 +1,118 @@
+/**
+ * DeBouncer module
+ */
+export var DeBouncer = {
+	/**
+	 * Run debounced function FIRST only ONCE per life of the debouncer
+	 */
+	onceFirst: function(func){
+		var _executedOnce = false;
+		return function(){
+			var context = this;
+			if (!_executedOnce && (_executedOnce = true)){
+				if (typeof func !== 'function')
+					arguments[0].apply(context);
+				else
+					func.apply(context,arguments);
+
+			}
+		}
+	},
+	/**
+	 * Run debounced function LAST only ONCE per life of the debouncer
+	 */
+	onceLast:function(func){
+		var _executedOnce = false;
+		return function(){
+			var context = this;
+			if (!_executedOnce && (_executedOnce = true)){
+				if (typeof func !== 'function')
+					arguments[0].apply(context);
+				else
+					func.apply(context,arguments);
+
+			}
+		}
+	},
+	/**
+	 * Run debounced function FIRST only ONCE per animation FRAME
+	 */
+	frameFirst: function(func){
+		var firstQueue = 0;
+		var firstQueueExecuted = false;
+		return function(){
+			var context = this;
+			if (!firstQueueExecuted && (firstQueueExecuted = true)){
+				if (typeof func !== 'function')
+					arguments[0].apply(context);
+				else
+					func.apply(context, arguments);
+			}
+			firstQueue ++;
+			window.requestAnimationFrame(function(){
+				firstQueue--
+				if (firstQueue==0){
+					firstQueueExecuted = false;
+				}
+			})
+		}
+	},
+	/**
+	 * Run debounced function LAST only ONCE per animation FRAME
+	 */
+	frameLast: function(func){
+		var lastQueue=0;
+		return function(){
+			lastQueue ++;
+			var context = this;
+			var args = arguments;
+			window.requestAnimationFrame(function(){
+				lastQueue--;
+				if (lastQueue==0){
+					if (typeof func !== 'function')
+						args[0].apply(context);
+					else
+						func.apply(context, args);
+				}
+			})
+		}
+	},
+	/**
+	 * Run debounced function FIRST only ONCE per TIMOUT
+	 */
+	timeoutFirst: function(timeoutMs, func){
+		var timeout;
+		return function(){
+			var context = this;
+
+			if (timeout){
+				clearTimeout(timeout);
+			}else{
+				if (typeof func !== 'function')
+					arguments[0].apply(context);
+				else
+					func.apply(context, arguments);
+			}
+			timeout = setTimeout(function(){clearTimeout(timeout)},timeoutMs);
+		}
+	},
+	/**
+	 * Run debounced function LAST only ONCE per TIMOUT
+	 */
+	timeoutLast: function(timeoutMs, func){
+		var timeout;
+		return function(){
+			var context = this;
+			var args = arguments;
+			if (timeout){
+				clearTimeout(timeout);
+			}
+			timeout = setTimeout(function(){
+				if (typeof func !== 'function')
+					args[0].apply(context);
+				else
+					func.apply(context, args);
+			},timeoutMs);
+		}
+	}
+}
--- core/helpers.js
+++ core/helpers.js
@@ -54,15 +54,6 @@ export function Override(context, method, callback){
 	return f;
 }
 
-/*Function.prototype['Override'] = function(func){
-	var superFunction = this;
-	return function(){
-		this.super = superFunction;
-		return func.apply(this,arguments);
-	};
-};*/
-
-
 export function GUID() {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
