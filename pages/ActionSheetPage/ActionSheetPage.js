18d81220c53f5e869107418233a6870e79f4430f
Eldar Gerfanov <zerodivide@mail.ru>
2018-11-21 15:47:03 -0500

    Added

---
+++ pages/ActionSheetPage/ActionSheetPage.js
@@ -0,0 +1,66 @@
+import './ActionSheetPage.scss';
+import { BasePage } from './../BasePage';
+
+/**
+ * @param {HTMLElement} container
+ */
+export class ActionSheetPage extends BasePage {
+
+	constructor(page){
+		super(page);
+		/** @type {{image:string,icon:string,title:string}[]} */
+		this.items = null;
+	}
+	/**
+	 * @param {*} d 
+	 */
+	slideDirection(d){
+		this.page.removeClass('direction-0');
+		this.page.removeClass('direction-1');
+		this.page.removeClass('direction-2');
+		this.page.removeClass('direction-3');
+		
+		this.page.addClass('direction-'+d);
+	}
+
+	onBackdropClicked(){
+		this.destroy();
+	}
+
+	_onItemClicked(item){
+		this.destroy()
+	}
+
+	onItemClicked(item){
+		throw new Error('Overwrite onItemClicked');
+	}
+
+	/**
+	 * Callback Returns true if the passed item is to be marker 'selected' in the list
+	 * @param {*} item 
+	 * @return {boolean}
+	 */
+	isSelectedItem(item){
+		return false;
+	}
+}
+ActionSheetPage.Direction={
+	Left: 0,
+	Top: 1,
+	Right: 2,
+	Up: 3,
+};
+ActionSheetPage.visibleParent = true;
+ActionSheetPage.className = "page-ActionSheetPage";
+ActionSheetPage.template = `
+<div class="backdrop" onclick="this.onBackdropClicked()"></div>
+<div class="menu-body">
+	<ul class="menu-tree">
+		<li [foreach]="index in this.items as item" onclick="this.onItemClicked(item); this._onItemClicked(item)" [selected] = "this.isSelectedItem(item)">
+			<i [class] = "item.icon" [if]="item.icon"></i>
+			<img bind = "item.image" [if]="item.image" />
+			<span [innerHTML] = "item.title"></span>
+		</li>
+	</ul>
+</div>
+`;
