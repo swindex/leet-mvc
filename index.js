1b88877650fd2ae588c8699116f1c316a9d9b36b
Eldar Gerfanov <zerodivide@mail.ru>
2018-11-22 12:39:10 -0500

    Added

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Map } from "leet-mvc/core/map";
+import { BasePage } from "leet-mvc/pages/BasePage";
+import { Objects } from "leet-mvc/core/Objects";
+import { NavController } from "leet-mvc/core/NavController";
+import { ConfirmButtons, Alert } from "leet-mvc/core/simple_confirm";
+import { Dialog, DialogPage } from "leet-mvc/pages/DialogPage/DialogPage";
+import { Translate, ReplaceValues } from "leet-mvc/core/Translate";
+import { Injector } from "leet-mvc/core/Injector";
+import { SimpleTabs } from "leet-mvc/components/SimpleTabs/SimpleTabs";
+import { MenuPage } from "leet-mvc/pages/MenuPage/MenuPage";
+import { ActionSheetPage } from "leet-mvc/pages/ActionSheetPage/ActionSheetPage";
+import { DeBouncer } from "leet-mvc/core/DeBouncer";
+import { DateTime } from "leet-mvc/core/DateTime";
+import { DataShape } from "leet-mvc/core/DataShape";
+import { CallbackQueue } from "leet-mvc/core/CallbackQueue";
+import { Analytics } from "leet-mvc/core/analytics_matomo";
+
