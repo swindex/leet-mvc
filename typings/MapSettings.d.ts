96fb0eb0aa105a4e2c928559fd63844964c94520
Eldar Gerfanov <zerodivide@mail.ru>
2019-04-04 11:25:03 -0400

    Directions Page uploaded

---
+++ typings/MapSettings.d.ts
@@ -0,0 +1,11 @@
+interface MapSettings{
+	disableDefaultUI: boolean,
+}
+interface DirectionSettings{
+	travelMode: string,
+	unitSystem: number,
+	avoidTolls: boolean,
+	avoidHighways: boolean,
+	avoidFerries: boolean,
+	provideRouteAlternatives: boolean,
+}
