48d694817b1d26d581feaffcf43828850c1eaee1
Eldar Gerfanov <zerodivide@mail.ru>
2019-11-07 15:54:21 -0500

    setup for vue

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -29,6 +29,7 @@ export class BasePage extends ChangeWatcher{
 		this.isVisible = null;
 		this.isDeleted = null;
 		this.isHidden = null;
+		this.isRoot = null,
 
 		this.selector = ""
 		this.className = this.constructor.className;
@@ -221,7 +222,7 @@ export class BasePage extends ChangeWatcher{
 	 *   You can extend base template by returning this.extendTemplate(super.template,'child template string');
 	 */
 	get template (){
-		return `<div page [class]="this.className" [style]="this.style" [attribute]="{hidden:this.isHidden,visible:this.isVisible,showing:this.isShowing,hiding:this.isHiding,creating:this.isCreating,deleting:this.isDeleting}">/*child-template*/</div>`;
+		return `<div page [class]="this.className" [style]="this.style" [attribute]="{root: this.isRoot, hidden:this.isHidden,visible:this.isVisible,showing:this.isShowing,hiding:this.isHiding,creating:this.isCreating,deleting:this.isDeleting}">/*child-template*/</div>`;
 	}
 }
 BasePage.visibleParent = null;
