24bc52da44b4b93e971f131be8767081e461c527
Eldar Gerfanov <zero.divide.net@gmail.com>
2020-04-13 19:57:43 -0400

    Validator and matomo fixes

--- core/analytics_matomo.js
+++ core/analytics_matomo.js
@@ -2,20 +2,22 @@ import { Objects } from "leet-mvc/core/Objects";
 
 // Analytics 
 export class Analytics {
-  static get Event() {return {
-    USERLOGIN:"Login",
-    APPERROR:"App Error",
-    LOADFILE: "Load File",
-    APIERROR: "API Error",
-    LOGINFAIL: "Login Failure"
-  };}
+  static get Event() {
+    return {
+      USERLOGIN: "Login",
+      APPERROR: "App Error",
+      LOADFILE: "Load File",
+      APIERROR: "API Error",
+      LOGINFAIL: "Login Failure"
+    };
+  }
 
-  static get Page(){
+  static get Page() {
     return {
       App: "Open App"
     };
   }
-	
+
   //User properties that do not change during visit. Used for Segmenting users
   static get UserProperty() {
     return {
@@ -38,27 +40,27 @@ export class Analytics {
 	 * Create Tracker instance
 	 * @param {{SiteId:number, URL: string}} config 
 	 */
-  constructor(config){
+  constructor(config) {
     this.config = config;
 
     //only init if device is initialized
-    if (typeof device !== 'undefined'){
-      this.Tracker = new Matomo(config.SiteId,config.URL);
+    if (typeof device !== 'undefined') {
+      this.Tracker = new Matomo(config.SiteId, config.URL);
     }
 
     this.PrevScreenName = null;
 
   }
 
-  init(){
-    this.Tracker = new Matomo(this.config.SiteId,this.config.URL);
+  init() {
+    this.Tracker = new Matomo(this.config.SiteId, this.config.URL);
   }
 
   /**
 	 * Enable/Disable analytics collection
 	 * @param {boolean} collectFlag 
 	 */
-  collectAnalytics (collectFlag){
+  collectAnalytics(collectFlag) {
     this.Tracker.enable(collectFlag);
   }
 
@@ -67,9 +69,9 @@ export class Analytics {
 	 * @param {string} name 
 	 * @param {string} optionalUrl
 	 */
-  setScreenName (name, optionalUrl=null){
+  setScreenName(name, optionalUrl = null) {
     this.Tracker.setScreenName(name);
-		
+
     var url = optionalUrl ? optionalUrl : name;
     this.Tracker.setURL(url);
     this.Tracker.setRefURL(this.PrevScreenName);
@@ -77,18 +79,18 @@ export class Analytics {
 
     this.Tracker.send();
     this.PrevScreenName = url;
-		
+
     return this;
   }
 
-  markNewVisit(){
+  markNewVisit() {
     this.Tracker.options.new_visit = 1;
   }
 
   /**
 	 * @param {string|number} id
 	 */
-  setUserId (id){
+  setUserId(id) {
     this.Tracker.setUserId(String(id));
     return this;
   }
@@ -97,11 +99,11 @@ export class Analytics {
 	 * @param {string|number} propertyName 
 	 * @param {string|number} propertyValue 
 	 */
-  setUserProperty (propertyName, propertyValue){
-    switch (propertyName){
-    //here we can intercept properties
-    default:
-      this.Tracker.setUserProperty(propertyName,propertyValue);
+  setUserProperty(propertyName, propertyValue) {
+    switch (propertyName) {
+      //here we can intercept properties
+      default:
+        this.Tracker.setUserProperty(propertyName, propertyValue);
     }
     return this;
   }
@@ -110,8 +112,8 @@ export class Analytics {
 	 * @param {string|number} propertyName 
 	 * @param {string|number} propertyValue 
 	 */
-  setActionProperty (propertyName, propertyValue){
-    this.Tracker.setActionProperty(propertyName,propertyValue);
+  setActionProperty(propertyName, propertyValue) {
+    this.Tracker.setActionProperty(propertyName, propertyValue);
     return this;
   }
   /**
@@ -119,15 +121,15 @@ export class Analytics {
 	 * @param {string|number} propertyName 
 	 * @param {string|number} propertyValue 
 	 */
-  setPersistentActionProperty (propertyName, propertyValue){
-    this.Tracker.setPersistentActionProperty(propertyName,propertyValue);
+  setPersistentActionProperty(propertyName, propertyValue) {
+    this.Tracker.setPersistentActionProperty(propertyName, propertyValue);
     return this;
   }
 
   /**
 	 * 
 	 */
-  clearPersistentActionProperties (){
+  clearPersistentActionProperties() {
     this.Tracker.PersistentActionVar = {};
     return this;
   }
@@ -138,7 +140,7 @@ export class Analytics {
 	 * @param {string|number} propertyName 
 	 * @param {string|number} propertyValue 
 	 */
-  setVisitDimension (propertyName, propertyValue){
+  setVisitDimension(propertyName, propertyValue) {
     this.Tracker.setActionDimension(propertyName, propertyValue);
   }
   /**
@@ -146,13 +148,13 @@ export class Analytics {
 	 * @param {string|number} propertyName 
 	 * @param {string|number} propertyValue 
 	 */
-  setActionDimension (propertyName, propertyValue){
+  setActionDimension(propertyName, propertyValue) {
     this.Tracker.setActionDimension(propertyName, propertyValue);
   }
   /**
 	 * Call after setting all user/visit related data
 	 */
-  logVisit(){
+  logVisit() {
     this.Tracker.send();
     return this;
   }
@@ -162,31 +164,31 @@ export class Analytics {
 	 * @param {string|number} eventName 
 	 * @param {any} eventValue 
 	 */
-  logEvent (eventName, eventValue = null){
+  logEvent(eventName, eventValue = null) {
     //console.log("eventName",eventValue);
-    if (eventName === null){
+    if (eventName === null) {
       //if mapping is set to null then dont track
       return;
     }
-    switch (eventName){
-    case Analytics.Event.SEARCH:
-      this.Tracker.setActionSearch(eventValue.search_value, eventValue.result_count, eventValue.category_name);
-      break;
-    case Analytics.Event.LOADFILE:
-      this.Tracker.setActionDownload(eventValue);
-      break;
-    case Analytics.Event.APPERROR:
-      this.Tracker.setEvent("Error",eventName,eventValue);
-      break;
-    case Analytics.Event.APIERROR:
-      this.Tracker.setEvent("Error",eventName, eventValue);
-      break;
-    case Analytics.Event.LOGINFAIL:
-      this.Tracker.setEvent("Error",eventName, eventValue);
-      break;	
+    switch (eventName) {
+      case Analytics.Event.SEARCH:
+        this.Tracker.setActionSearch(eventValue.search_value, eventValue.result_count, eventValue.category_name);
+        break;
+      case Analytics.Event.LOADFILE:
+        this.Tracker.setActionDownload(eventValue);
+        break;
+      case Analytics.Event.APPERROR:
+        this.Tracker.setEvent("Error", eventName, eventValue);
+        break;
+      case Analytics.Event.APIERROR:
+        this.Tracker.setEvent("Error", eventName, eventValue);
+        break;
+      case Analytics.Event.LOGINFAIL:
+        this.Tracker.setEvent("Error", eventName, eventValue);
+        break;
       //here we can intercept properties
-    default:
-      this.Tracker.setEvent("Event", eventName, eventValue);
+      default:
+        this.Tracker.setEvent("Event", eventName, eventValue);
     }
     this.Tracker.send();
     return this;
@@ -195,53 +197,52 @@ export class Analytics {
 
 
 class Matomo {
-  constructor(id, url){
+  constructor(id, url) {
     //set properties
     this._URL = url;
     this._enabled = true;
     /**
 		 * @type {MatomoTrackOptions}
 		 */
-    this.options ={
+    this.options = {
       url: null,
-      idsite : id,
-      rec : 1,
+      idsite: id,
+      rec: 1,
       apiv: 1,
-      send_image:0,
-      new_visit : 1,
-      res : window.screen.width + "x" + window.screen.height
+      send_image: 0,
+      new_visit: 1,
+      res: window.screen.width + "x" + window.screen.height
     };
 
     this.ActionOpts = {};
 
-    this.EventVar={};
+    this.EventVar = {};
 
-    this.UserVar ={
-      OS:  device ? device.platform : null,
-      Model:device ? device.model : null,
-      Version: null
+    this.UserVar = {
+      OS: device ? device.platform : null,
+      Model: device ? device.model : null,
     };
-	
-    this.ActionVar ={};
-    this.ActionDimension ={};
+
+    this.ActionVar = {};
+    this.ActionDimension = {};
     this.VisitDimension = {};
 
     this.PersistentActionVar = {};
   }
 
-  enable(flag=true){
-    this._enabled= flag;
+  enable(flag = true) {
+    this._enabled = flag;
   }
 
-  setScreenName(name){
+  setScreenName(name) {
     this.options.action_name = name;
     return this;
   }
-  setURL(name){
+  setURL(name) {
     this.options.url = name;
     return this;
   }
-  setRefURL(name){
+  setRefURL(name) {
     this.options.urlref = name;
     return this;
   }
@@ -250,7 +251,7 @@ class Matomo {
 	 * 
 	 * @param {number} ver 
 	 */
-  setAppVersion(ver){
+  setAppVersion(ver) {
     this.UserVar.Version = ver;
     return this;
   }
@@ -258,33 +259,33 @@ class Matomo {
 	 * 
 	 * @param {string} uid 
 	 */
-  setUserId(uid){
+  setUserId(uid) {
     this.options.uid = uid;
     return this;
   }
 
-  setUserProperty(user_property, property_value){
+  setUserProperty(user_property, property_value) {
     this.UserVar[user_property] = property_value;
   }
 
-  setActionProperty(action_property, action_value){
+  setActionProperty(action_property, action_value) {
     this.ActionVar[action_property] = action_value;
   }
 
-  setPersistentActionProperty(action_property, action_value){
+  setPersistentActionProperty(action_property, action_value) {
     this.PersistentActionVar[action_property] = action_value;
   }
 
-  clearPersistentActionProperties(){
+  clearPersistentActionProperties() {
     this.PersistentActionVar = {};
   }
 
-  setActionDimension(action_property, action_value){
-    this.ActionDimension["dimension"+action_property] = action_value;
+  setActionDimension(action_property, action_value) {
+    this.ActionDimension["dimension" + action_property] = action_value;
   }
 
-  setVisitDimension(action_property, action_value){
-    this.VisitDimension["dimension"+action_property] = action_value;
+  setVisitDimension(action_property, action_value) {
+    this.VisitDimension["dimension" + action_property] = action_value;
   }
 
   /**
@@ -294,26 +295,26 @@ class Matomo {
 	 * @param {*} [eventName]  The event name. (eg. a Movie name, or Song name, or File name...)
 	 * @param {*} [eventValue] The event value. Must be a float or integer value (numeric), not a string.
 	 */
-  setEvent(eventCategory , eventAction, eventName = null, eventValue = null){
-    this.EventVar={
-      e_c:eventCategory,
-      e_a:eventAction,
-      e_n:eventName,
-      e_v:eventValue
+  setEvent(eventCategory, eventAction, eventName = null, eventValue = null) {
+    this.EventVar = {
+      e_c: eventCategory,
+      e_a: eventAction,
+      e_n: eventName,
+      e_v: eventValue
     };
   }
 
-  setActionDownload(action_value){
+  setActionDownload(action_value) {
     this.ActionOpts.download = action_value;
     this.ActionOpts.url = action_value;
   }
 
-  setActionOpenLink(action_value){
+  setActionOpenLink(action_value) {
     this.ActionOpts.link = action_value;
     this.ActionOpts.url = action_value;
   }
 
-  setActionSearch(search_value, result_count = 0, category_name = null){
+  setActionSearch(search_value, result_count = 0, category_name = null) {
     this.ActionOpts.search = search_value;
     this.ActionOpts.search_count = result_count;
     this.ActionOpts.search_cat = category_name;
@@ -324,15 +325,15 @@ class Matomo {
 	 * @param {KeyValuePair} cvarObj
 	 * @return {string}
 	*/
-  _createCVAR(cvarObj){
+  _createCVAR(cvarObj) {
 
     var uv = Object.keys(cvarObj);
     var cv = {};
-    for (var i=0; i<uv.length; i++){
+    for (var i = 0; i < uv.length; i++) {
       //if (cvarObj[uv[i]]!==null)
-      cv[i+1]=[uv[i],cvarObj[uv[i]]];
+      cv[i + 1] = [uv[i], cvarObj[uv[i]]];
     }
-    var ret = JSON.stringify( cv );	
+    var ret = JSON.stringify(cv);
     return ret;
   }
 
@@ -340,45 +341,45 @@ class Matomo {
 	 * Send collected info to analytics.
 	 * Resets the Action options
 	 */
-  send(){
+  send() {
     if (!this._enabled)
       return;
-    if (!this._URL)	
+    if (!this._URL)
       return;
     //set up visit vars	
     this.options._cvar = this._createCVAR(this.UserVar);
-		
-    //set up event vars	
-    this.options.cvar = this._createCVAR( Object.assign({},this.ActionVar,this.PersistentActionVar ));
 
-    var payload = Object.assign({}, this.options,this.ActionOpts, this.EventVar, this.ActionDimension, this.VisitDimension);
+    //set up event vars	
+    this.options.cvar = this._createCVAR(Object.assign({}, this.ActionVar, this.PersistentActionVar));
+
+    var payload = Object.assign({}, this.options, this.ActionOpts, this.EventVar, this.ActionDimension, this.VisitDimension);
     //clear action values right away to prevent another request picking them up before this one completes!
     this.clearActionValues();
     //console.log(pay);
 
     var formData = new FormData();
 
-    Objects.forEach(payload, (value, key)=>{
-      formData.set(key, JSON.stringify(value));
-    });		
+    Objects.forEach(payload, (value, key) => {
+      formData.set(key, value);
+    });
 
     window.fetch(this._URL,
       {
         method: 'POST',
         //headers: new Headers([['Content-Type','application/json']]),
-        mode:'no-cors',
+        mode: 'no-cors',
         body: formData
-      }).then((ret)=>{
-      //console.log("SPY OK");
-    }).catch((err)=>{
-      //console.log("SPY Err:" + JSON.stringify(this._URL)+ JSON.stringify(payload) + JSON.stringify(err));
-    });
+      }).then((ret) => {
+        //console.log("SPY OK");
+      }).catch((err) => {
+        //console.log("SPY Err:" + JSON.stringify(this._URL)+ JSON.stringify(payload) + JSON.stringify(err));
+      });
   }
   /**
 	 * Set new visit to 0
 	 * Reset Action and Event parameters
 	 */
-  clearActionValues(){
+  clearActionValues() {
     this.options.new_visit = 0;
     this.ActionVar = {};
     this.ActionOpts = {};
